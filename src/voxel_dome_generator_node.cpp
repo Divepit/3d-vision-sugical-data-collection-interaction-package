31917660bb133219a5fb1de6c6b6e7c8fe67bc5d
Marco Trentini <marcotr@student.ethz.ch>
2023-04-27 15:18:18 +0200

    fix push

--- src/voxel_dome_generator_node.cpp
+++ src/voxel_dome_generator_node.cpp
@@ -3,7 +3,7 @@
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <sdc_interaction/UpdateVoxelDome.h> 
-#include <sdc_interaction/UpdateObstacles.h>
+#include <sdc_interaction/UpdateObstacles.h> 
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <functional>
